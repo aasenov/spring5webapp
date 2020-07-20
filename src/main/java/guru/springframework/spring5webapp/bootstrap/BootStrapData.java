f4bdadf51297e880ce9fe2e5560765af6317ca86
Asen Asenov <aasenov.bg@gmail.com>
2020-07-20 17:06:12 +0300

    Add publisher entity and test storing of objects

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher ss = new Publisher("Simon & Schuster","New York","NY",10020,"1230 Avenue of the Americas " );
+        publisherRepository.save(ss);
+
+        System.out.println("Number of publishers: "+publisherRepository.count());
+
+        Author erik = new Author("Erik", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+
+        erik.getBooks().add(ddd);
+        ddd.getAuthors().add(erik);
+
+        authorRepository.save(erik);
+        bookRepository.save(ddd);
+
+        Author rdd = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "12312312321321");
+        rdd.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rdd);
+        authorRepository.save(rdd);
+        bookRepository.save(noEJB);
+
+        System.out.println("Number of books: "+bookRepository.count());
+
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,111 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String city;
+    private String state;
+    private int zip;
+    private String addressLine1;
+
+    public Publisher(){
+
+    }
+
+    public Publisher(String name, String city, String state, int zip, String addressLine1) {
+        this.name = name;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.addressLine1 = addressLine1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Publisher{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", state='").append(state).append('\'');
+        sb.append(", zip=").append(zip);
+        sb.append(", address='").append(addressLine1).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+}
